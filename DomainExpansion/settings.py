715837cbc373cff363e92e94f4772f3a3aa0da53
techonair <techionmail@gmail.com>
2022-08-30 22:30:50 +0530

    settings configured for Heroku

--- DomainExpansion/settings.py
+++ DomainExpansion/settings.py
@@ -51,7 +51,7 @@ AUTH_USER_MODEL ='base.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
