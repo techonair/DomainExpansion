d9b8de3711fd98577c064fc80d7c95d5830a9507
techonair <techionmail@gmail.com>
2022-09-04 03:06:28 +0530

    .

--- DomainExpansion/urls.py
+++ DomainExpansion/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve 
 
-settings.configure()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
