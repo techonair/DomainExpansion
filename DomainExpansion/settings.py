da194b0409d26809fc648472a2301a5faacf8fe0
techonair <techionmail@gmail.com>
2022-09-04 03:18:01 +0530

    .

--- DomainExpansion/settings.py
+++ DomainExpansion/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/images/'
+MEDIA_URL = BASE_DIR  / 'static/images/'
 
 
 STATICFILES_DIRS = [
