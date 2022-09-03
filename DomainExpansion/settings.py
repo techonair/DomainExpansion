bfc75b74a6607a553a20f068a4a1a44b23210a44
techonair <techionmail@gmail.com>
2022-09-03 17:23:22 +0530

    Static Root added / Static file Problem Solved

--- DomainExpansion/settings.py
+++ DomainExpansion/settings.py
@@ -130,10 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = 'staticfiles/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -141,7 +143,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
