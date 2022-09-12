75a3dd4b2503dca3a9e719b4bb348867c6af76ed
techonair <techionmail@gmail.com>
2022-09-12 21:41:33 +0530

    Bug related to profile pic fixed

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
-    avatar = models.ImageField(null=True, default='avatar.svg')
+    avatar = models.ImageField(null=True, blank=True, default='/avatar.svg')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
