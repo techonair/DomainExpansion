7cfb26adb61cbfe042a313605593cb139209c467
techonair <techionmail@gmail.com>
2022-09-14 18:49:02 +0530

    Server Error test

--- DomainExpansion/settings.py
+++ DomainExpansion/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join('/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
