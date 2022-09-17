246cb91e3ec4ee4a9aa5f2eb16efb447a49219b6
Bhanu Pratap <83609706+techonair@users.noreply.github.com>
2022-09-17 12:13:14 +0530

    Update settings.py

--- DomainExpansion/settings.py
+++ DomainExpansion/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*=kcs^a3sbu_$v$=((hl+53u4p+*%r5aj+x&$sxjl$u_i!crkr'
-# SECRET_KEY = os.environ['SECRET_KEY']
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
